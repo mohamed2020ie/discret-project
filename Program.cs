d12cd5f5e16e7557e9e6cf2ae417e8e4dbd45adf
mohamed omar <111253955+mohamed2020ie@users.noreply.github.com>
2022-12-18 20:27:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the start");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the end");
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine("the prime numbers are : ");
+          
+            for (int i = start; i <= end; i++)
+            {
+                bool isprime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isprime = false;
+                        break;
+                    }
+                }
+                if(i==1) continue; 
+                if (i==0) continue;
+                if (isprime)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
